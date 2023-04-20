52cdf577588e415917bc7916ea4beb9101c54d57
Estéban SOUBIRAN (ANEO) <esoubiran@aneo.fr>
2023-04-20 22:51:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ArmoniK C# API Docs
+
+## Close C# projects
+
+```bash
+git clone https://github.com/aneoconsulting/ArmoniK.Core.git ArmoniK.Core
+git clone https://github.com/aneoconsulting/ArmoniK.Extensions.Csharp.git ArmoniK.Extensions.Csharp
+git clone https://github.com/aneoconsulting/ArmoniK.Utils.git ArmoniK.Utils
+```
---
+++ scripts/clone-projects.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clone ArmoniK.Core
+git clone https://github.com/aneoconsulting/ArmoniK.Core.git ArmoniK.Core
+
+# Clone ArmoniK.Extensions.Csharp
+git clone https://github.com/aneoconsulting/ArmoniK.Extensions.Csharp.git ArmoniK.Extensions.Csharp
+
+# Clone ArmoniK.Extensions.Utils
+git clone https://github.com/aneoconsulting/ArmoniK.Utils.git ArmoniK.Utils
